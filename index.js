7410a4b6831725cdbdbe7cc1718ed7c5b6521906
Ataur-rahman-01 <iamataur0911@gmail.com>
2021-05-26 14:14:31 +0600

    gjhg

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 // });
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://eloquent-meninsky-b5c3a0.netlify.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
